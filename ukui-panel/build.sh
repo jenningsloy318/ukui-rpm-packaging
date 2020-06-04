c26a9dbd6734e4450fcf2353215d7151843f6134
Jennings Liu <jennings.liu@sap.com>
2020-06-04 13:45:57 +0800

    fix package name error

--- ukui-panel/build.sh
+++ ukui-panel/build.sh
@@ -2,9 +2,9 @@
 
 echo "install build dependencies"
 dnf install /root/peony-devel-2.1.2-1.fc32.x86_64.rpm  /root/peony-libs-2.1.2-1.fc32.x86_64.rpm -y
-dnf install -y $(grep  BuildRequires /root/ukui-pannel.spec |awk '{print $2}')
+dnf install -y $(grep  BuildRequires /root/ukui-panel.spec |awk '{print $2}')
 strip --remove-section=.note.ABI-tag /usr/lib64/libQt5Core.so.5
 
 echo "build rpm package"
 
-rpmbuild -ba /root/ukui-pannel.spec
+rpmbuild -ba /root/ukui-panel.spec
